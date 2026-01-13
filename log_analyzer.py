d63f35878b9f2e9d0da02b85097d9e85390e18da
ixteal <xunnnnn12345@gmail.com>
2026-01-13 21:14:32 +0800

    Update log_analyzer.py

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,1 +1,16 @@
+failed_attempts = 0
 
+with open("sample_logs.txt", "r") as file:
+  logs = file.readlines()
+
+for log in logs:
+  if "Failed" in log:
+    failed_attempts += 1
+    print ("Suspicious Detected:", log.strip())
+
+print("\nTotal failed attempts:", failed_attempts)
+
+if failed_attempts >= 3:
+  print("Brute force is used!)
+        else:
+  print("The System is secured")
